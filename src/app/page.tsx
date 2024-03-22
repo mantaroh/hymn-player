8f1c5cab0622672b2b42d770fb986c8d32cf4e15
Mantaroh Yoshinaga <mantaroh@gmail.com>
2024-03-22 11:06:35 +0900

    CORS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,20 @@ import { useRef, useState } from "react";
 
 export default function Home() {
   const [currentNo, setCurrentNo] = useState(1);
+  const audioRef = useRef<HTMLAudioElement>(null);
 
   const setMusic = async () => {
+    if (!audioRef.current) return;
     // https://jelc.or.jp/wp-content/uploads/sanbi-mid/005.mid
     console.log(currentNo);
-    const url = `https://jelc.or.jp/wp-content/uploads/sanbi-mid/${('0000'+currentNo).slice(-3)}.mid`;
+    const url = `http://hymn-player.vercel.app/api/get-hymn?no=${('0000'+currentNo).slice(-3)}`;
     const res = await fetch(url);
+    const blob = await res.blob();
+    const objectURL = URL.createObjectURL(blob);
+    console.log(objectURL);
+    audioRef.current.src = objectURL;
   }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -20,7 +27,7 @@ export default function Home() {
 
       <figure>
         <figcaption>賛美歌：{('0000'+currentNo).slice(-3)}番</figcaption>
-        <audio controls src="/media/cc0-audio/t-rex-roar.mp3"></audio>
+        <audio ref={audioRef} controls src="/media/cc0-audio/t-rex-roar.mp3"></audio>
         <a href="/media/cc0-audio/t-rex-roar.mp3"> Download audio </a>
       </figure>
     </main>
